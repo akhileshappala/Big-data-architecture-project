603eacf4abd6cffe46b5f94e3a7683b31fd3a8cc
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2024-04-28 11:58:02 -0600

    changes for the issue

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  userAPIURL: 'http://35.197.88.145',
-  donorAPIURL: 'http://34.82.141.247'
+  userAPIURL: 'https://35.197.88.145',
+  donorAPIURL: 'https://34.82.141.247'
 };
